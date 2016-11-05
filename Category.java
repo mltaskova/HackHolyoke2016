e677c65d87f4f5338dc5ecefe8cb4fb9d8eee02d
Maria-Luiza Taskova <mltaskova@yahoo.co.uk>
2016-11-05 11:21:21 -0400

    Add files via upload

--- Category.java
+++ Category.java
@@ -1,18 +1,131 @@
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 public class Category {
 
 	private DoublyLinkedListImpl<Flashcard> myList;
-	
-	public Category () {		
+	//instance variables for the database connection!!
+	 private Connection connect = null;
+     private Statement statement = null;
+     private PreparedStatement preparedStatement = null;
+     private ResultSet resultSet = null;
+	 private String name;
+	public Category (String name) {		
+		this.name = name;
 		myList = new DoublyLinkedListImpl<Flashcard>();
+		
 	}
 	
-	public void insert(Flashcard myFlashcard) {
+	public void insert(Flashcard myFlashcard) throws Exception {
+		try {
+			 // This will load the MySQL driver, each DB has its own driver
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        // Setup the connection with the DB
+	        connect = DriverManager
+	                        .getConnection("jdbc:mysql://127.0.0.1:3306/catagories?"
+	                                        + "user=root&password=king");
+	        
+	     // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement
+                            .executeQuery("select * from catagories.japanese");
+            writeResultSet(resultSet);
+	        
+	        preparedStatement = connect
+                    .prepareStatement("insert into  catagories.japanese (word, definition) values (?, ?)");
+	        preparedStatement.setString(1, myFlashcard.getWord());
+	        preparedStatement.setString(2, myFlashcard.getDefinition());
+	        
+	        preparedStatement.executeUpdate();
+	        
+	        //testing purposes
+	        resultSet = statement
+                    .executeQuery("select * from catagories.japanese");
+                    writeMetaData(resultSet);
+		}
+		catch (Exception e) {
+            throw e;
+            }
+		
+
 		myList.addFirst(myFlashcard);			
 	}
+	/*
+	 * temporary (I HOPE)
+	 */
+	 private void writeMetaData(ResultSet resultSet) throws SQLException {
+         //         Now get some metadata from the database
+         // Result set get the result of the SQL query
+
+         System.out.println("The columns in the table are: ");
+
+         System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+         for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
+                 System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+         }
+	 }
+	 /*
+		 * temporary (I HOPE)
+		 */
+	 private void writeResultSet(ResultSet resultSet) throws SQLException {
+         // ResultSet is initially before the first data set
+         while (resultSet.next()) {
+                 // It is possible to get the columns via name
+                 // also possible to get the columns via the column number
+                 // which starts at 1
+                 // e.g. resultSet.getSTring(2);
+                 String word = resultSet.getString("word");
+                 String definition = resultSet.getString("definition");
+                 System.out.println("word: " + word);
+                 System.out.println("definition: " + definition);
+         }
+	 }
 	
-	public void delete(Node<Flashcard> current) {
+	public void delete(Node<Flashcard> current) throws Exception {
+		try {
+		 // This will load the MySQL driver, each DB has its own driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // Setup the connection with the DB
+        connect = DriverManager
+                        .getConnection("jdbc:mysql://127.0.0.1:3306/catagories?"
+                                        + "user=root&password=king");
+        
+     // Statements allow to issue SQL queries to the database
+        statement = connect.createStatement();
+        // Result set get the result of the SQL query
+        resultSet = statement
+                        .executeQuery("select * from catagories.japanese");
+        writeResultSet(resultSet);
+        
+        preparedStatement = connect
+                .prepareStatement("delete from  catagories.japanese where word = ?");
+        preparedStatement.setString(1, current.getElement().getWord());
+//        preparedStatement.setString(2, myFlashcard.getDefinition());
+        
+        preparedStatement.executeUpdate();
+        
+        //testing purposes
+        resultSet = statement
+                .executeQuery("select * from catagories.japanese");
+                writeMetaData(resultSet);
+		}
+                
+	catch (Exception e) {
+        throw e;
+        }
+		
 		myList.remove(current);
 	}
 	
@@ -32,29 +145,31 @@ public class Category {
 		return (Flashcard)curNode.getNext().getElement();
 	}
 
-	public Node getLast() {
+	public Node<Flashcard> getLast() {
 		return myList.getLast();
 	}
 	
-	public Node getFirst() {
+	public Node<Flashcard> getFirst() {
 		return myList.getFirst();
 	}
 	
-	public static void main(String args[])
+	public static void main(String args[]) throws Exception
 	{
-		Category myCategory = new Category ();
-		myCategory.insert(new Flashcard ("aaa", "aaa"));
-		myCategory.insert(new Flashcard ("bbb", "bbb"));
-		myCategory.insert(new Flashcard ("ccc", "ccc"));
-		myCategory.insert(new Flashcard ("ddd", "ddd"));
-		myCategory.insert(new Flashcard ("eee", "eee"));
-//		myCategory.delete(myCategory.getLast());
-//	    myCategory.iterateBackward();
-//	    myCategory.iterateForward();
-    	myCategory.delete(myCategory.getFirst().getNext());
-		myCategory.delete(myCategory.getLast().getPrev().getPrev());
-		myCategory.iterateForward();
-		myCategory.iterateBackward();
-		myCategory.prev(myCategory.getLast());
+//		Category myCategory = new Category ("japanese");
+//		myCategory.insert(new Flashcard ("cat", "animal"));
+//		
+//		myCategory.delete(myCategory.getFirst());
+//		myCategory.insert(new Flashcard ("bbb", "bbb"));
+//		myCategory.insert(new Flashcard ("ccc", "ccc"));
+//		myCategory.insert(new Flashcard ("ddd", "ddd"));
+//		myCategory.insert(new Flashcard ("eee", "eee"));
+////		myCategory.delete(myCategory.getLast());
+////	    myCategory.iterateBackward();
+////	    myCategory.iterateForward();
+//    	myCategory.delete(myCategory.getFirst().getNext());
+//		myCategory.delete(myCategory.getLast().getPrev().getPrev());
+//		myCategory.iterateForward();
+//		myCategory.iterateBackward();
+//		myCategory.prev(myCategory.getLast());
 	}
 }
